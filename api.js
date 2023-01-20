8cab99092beb6e091b6d99d4dc688b860f687ea1
Ben Orndorff <btorndorff@gmail.com>
2023-01-20 17:35:09 -0500

    cors

--- api.js
+++ api.js
@@ -19,11 +19,11 @@ client.connect(err => {
 app.use(cors())
 app.options('*', cors())
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
 // helper functions
 
